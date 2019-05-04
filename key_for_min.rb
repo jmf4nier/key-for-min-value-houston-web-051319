e418ef85ddbb56910775b302d3465fcf3ce43bb7
Jason Fournier <jmf4nier@hotmail.com>
2019-05-04 19:16:34 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -8,7 +8,9 @@ ikea = {:chair => 25, :table => 85, :mattress => 450}
 def key_for_min_value(name_hash)
   name_hash.collect do |key, value|
     new_value = value[0]
-    new_value <=> value
+    if new_value <=> value == 1
+
+
   end
 end
 
