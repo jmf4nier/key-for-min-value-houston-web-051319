ef3286a53929b2908b4d75880f662be15484b927
Jason Fournier <jmf4nier@hotmail.com>
2019-05-04 19:31:46 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -6,12 +6,14 @@
 ikea = {:chair => 25, :table => 85, :mattress => 450}
 
 def key_for_min_value(name_hash)
-  array = []
+  lowest_value = nil
+  lowest_key = nil
   name_hash.each do |key, value|
-    array.push(value)
+    if lowest_value == nil || value < lowest_value
+      lowest_value = value
+      lowest_key = key
   end
-  array
-    array.select
+  lowest_key
 end
 
 key_for_min_value(ikea)
